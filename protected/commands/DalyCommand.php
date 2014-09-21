85d06b8539717b576f8197dfd555bdc854f0a3e7
andrew2net <andriano@ngs.ru>
2014-09-22 02:36:26 +0700

    Revert ""
    This reverts commit 360a6c9ffd723bd362b8a048fb30605b2af4bbe9.

--- protected/commands/DalyCommand.php
+++ protected/commands/DalyCommand.php
@@ -7,7 +7,7 @@
 class DalyCommand extends CConsoleCommand {
 
   public function run($args) {
-      echo 'Cron daly ';
+//      echo 'Cron daly ';
 //    global $argv;
 //    $conn = explode('=', $argv[2]);
 //    Yii::trace('Start daly ' . $conn[1], 'cron');
