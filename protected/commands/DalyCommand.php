66e5a4befb96034528324bbc1fa29b4d458f0c4a
andrew2net <andriano@ngs.ru>
2014-10-07 11:15:38 +0700

    Revert ""
    This reverts commit 360a6c9ffd723bd362b8a048fb30605b2af4bbe9.

--- protected/commands/DalyCommand.php
+++ protected/commands/DalyCommand.php
@@ -40,38 +40,4 @@ class DalyCommand extends CConsoleCommand {
     }
   }
 
-  private function getCurrencyRate() {
-    $wsdl = 'http://www.cbr.ru/dailyinfowebserv/dailyinfo.asmx?WSDL';
-    $client = new SoapClient($wsdl);
-    $params["On_date"] = date('Y-m-d');
-    $result = $client->GetCursOnDate($params);
-    
-    $xml = new SimpleXMLElement($result->GetCursOnDateResult->any);
-    $currencyData = $xml->xpath("//ValuteCursOnDate[VchCode='KZT']");
-    if (!isset($currencyData[0]))
-      return;
-    
-    $rate = (string) $currencyData[0]->Vcurs;
-    $quantity = (string) $currencyData[0]->Vnom;
-    
-    Yii::import('application.modules.payments.models.CurrencyRate');
-    $currencyRate = CurrencyRate::model()->findByAttributes(array(
-      'date' => $params['On_date'],
-      'from' => 'KZT',
-      'to' => 'RUB',
-      ));
-    if (is_null($currencyRate)){
-      $currencyRate = new CurrencyRate;
-      $currencyRate->date = $params['On_date'];
-      $currencyRate->from = 'KZT';
-      $currencyRate->to = 'RUB';
-    }
-    $currencyRate->from_quantity = $quantity;
-    $currencyRate->to_quantity = 1;
-    $currencyRate->rate = $rate;
-    $currencyRate->save();
-  }
-
 }
-
-?>
