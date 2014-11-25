457fd3a03cc66499ce9c11c48c6e4884dd41db80
andrew2net <andriano@ngs.ru>
2014-11-26 00:22:53 +0700

    Revert ""
    This reverts commit 360a6c9ffd723bd362b8a048fb30605b2af4bbe9.

--- themes/tornado/views/layouts/main.php
+++ themes/tornado/views/layouts/main.php
@@ -1,9 +1,9 @@
 <?php /* @var $this Controller */ ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//RU" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="ru">
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ru" lang="ru">
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta name="language" content="ru" />
+    <!--<meta name="language" content="ru" />-->
     <link rel="icon" href="<?php echo Yii::app()->theme->baseUrl; ?>/img/favicon.png" type="image/x-icon" />
     <!--<link rel="shortcut icon" href="<?php // echo Yii::app()->createAbsoluteUrl('');         ?>/favicon.ico" type="image/x-icon" />-->
     <!-- blueprint CSS framework -->
