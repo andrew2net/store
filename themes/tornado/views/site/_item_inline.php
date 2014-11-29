cba2a8276d9fd8dea72772cedbe0b24020f42ca2
andrew2net <andriano@ngs.ru>
2014-11-30 04:30:30 +0700

    Revert ""
    This reverts commit 360a6c9ffd723bd362b8a048fb30605b2af4bbe9.

--- themes/tornado/views/site/_item_inline.php
+++ themes/tornado/views/site/_item_inline.php
@@ -48,7 +48,7 @@ $webUser =Yii::app()->user;
 $user = User::model()->with(array('customerProfile' => array('with' => 'price')))->findByPk($webUser->id);
 $wholesalePrices = array();
 foreach ($data->prices as $p) {
-  if ($p->price && ($webUser->isGuest || $p->price_type->summ > $user->customerProfile->price->summ))
+  if ($p->price && ($webUser->isGuest || $user->customerProfile->price && $p->price_type->summ > $user->customerProfile->price->summ))
     $wholesalePrices[] = array(
       $p->price_type->summ,
       $p->price,
