5599f3968e78cd5b66bbf4c5f97077247817142a
andrew2net <andriano@ngs.ru>
2014-12-11 18:55:13 +0700

    Revert ""
    This reverts commit 360a6c9ffd723bd362b8a048fb30605b2af4bbe9.

---
+++ themes/tornado/views/site/_news.php
@@ -0,0 +1,3 @@
+<?php if (new DateTime < new DateTime('2015/01/01')) { ?>
+<div class="yellow-border blue" style="padding: 10px; margin-bottom: 10px; text-align: center"><span class="red">Внимание АКЦИЯ!</span> Бесплатная доставка заказов оплаченных до 1 января 2015 года <span style="font-size: 8pt; color: #555">(* кроме авиа доставки)</span>.</div>
+<?php } ?>
