b4be7e5b2475559dba8df898c32de5fadc68a002
andrew2net <andriano@ngs.ru>
2014-12-23 23:36:14 +0700

    Revert ""
    This reverts commit 360a6c9ffd723bd362b8a048fb30605b2af4bbe9.

--- protected/modules/catalog/models/Category.php
+++ protected/modules/catalog/models/Category.php
@@ -167,16 +167,17 @@ class Category extends CActiveRecord {
     return parent::beforeSave();
   }
 
-  public function hasProducts($root=null, $level = 1) {
+  public function hasProducts($root = null, $level = 1) {
 
     $this->getDbCriteria()->mergeWith(array(
       'with' => array('subcategories' => array(
           'select' => 'subcategories.id',
-          'join' => 'LEFT JOIN store_product_category pc ON pc.category_id IN (t.id, subcategories.id)',
+          'join' => 'LEFT JOIN store_product_category pc ON pc.category_id IN (t.id, subcategories.id) ' .
+          'LEFT JOIN store_product prod on prod.id=pc.product_id',
         )),
       'select' => 't.id, t.name, t.lft, t.rgt, t.root, COUNT(pc.product_id) AS count_products',
       'order' => 't.lft',
-      'condition' => 't.level=:level AND (t.root=:root OR :root IS NULL)',
+      'condition' => 't.level=:level AND (t.root=:root OR :root IS NULL) AND prod.show_me=1 AND prod.remainder>0',
       'group' => 't.id',
       'together' => 'true',
       'having' => 'count_products>0',
