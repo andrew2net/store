f101f6463f90f582ac64270f5d519a5c6f57995a
andrew2net <android.2net@gmail.com>
2015-07-09 22:47:13 +0600

    payment

--- themes/tornado/views/pay/result.php
+++ themes/tornado/views/pay/result.php
@@ -1,6 +1,6 @@
 <?php $this->pageTitle = Yii::app()->name . ' - Оплата заказа'; ?>
 <div class="container" id="page">
-  <h1 class="cufon bold green" style="margin-top: 40px">Оплата прошла успешно</h1>
+  <h1 class="bold blue" style="margin-top: 40px">Оплата прошла успешно</h1>
   <div>
     <?php echo CHtml::link('Вернуться на главную страницу', '/'); ?><br>
     <?php echo CHtml::link('Личный кабинет', '/profile'); ?>
