bb07c31c696b8ee0da6907f02dc47c20b816dd45
andrew2net <android.2net@gmail.com>
2015-11-25 16:43:43 +0600

    Remove Hyundai

--- themes/tornado/views/site/_newsItemMini.php
+++ themes/tornado/views/site/_newsItemMini.php
@@ -3,5 +3,5 @@
 ?>
 <?php echo $data->date; ?>
 <p<?php echo (isset($news_id) && $data->id == $news_id ? ' class="bold"' : ''); ?>>
-    <a href="<?php echo $this->createUrl('news/item', ['id' => $data->id]); ?>" rel="nofollow"><?php echo $data->title; ?></a>
+    <a href="<?php echo $this->createUrl('news/item', ['id' => $data->id]); ?>"><?php echo $data->title; ?></a>
 </p>
