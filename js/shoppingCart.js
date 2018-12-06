8afc1793d0baaa01d607e8a66da00aa4ba41a1f3
Andrei Kislichenko <andrei.kislichenko@gmail.com>
2018-12-06 14:41:34 +0100

    Update shoppingCart.js

--- js/shoppingCart.js
+++ js/shoppingCart.js
@@ -112,7 +112,7 @@ $(document).ready(function () {
                     'eventAction': 'createorder',
                     'eventValue': summTotal
                 });
-                $('form.item-submit').submit();
+                $('form#item-submit').submit();
             } else {
                 cartProc.hide();
                 cartSubmit.show();
